676eb2301ee889570524fc3bf37e0e74bb2bff39
George Whiting <george_whiting@hotmail.com>
2018-02-08 16:35:24 +0000

    add unique segment of image to each block

--- Scrambled Jpeggs/ViewController.swift
+++ Scrambled Jpeggs/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     
     var blockArray: NSMutableArray = []
     var centersArray: NSMutableArray = []
-    
+    var picNum : Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +35,10 @@ class ViewController: UIViewController {
         xCenter = blockWidth / 2
         yCenter = blockWidth / 2
         
+        picNum = 0
+        
+        let image = [#imageLiteral(resourceName: "Image1"), #imageLiteral(resourceName: "Image2"), #imageLiteral(resourceName: "Image3"), #imageLiteral(resourceName: "Image4"), #imageLiteral(resourceName: "Image5"), #imageLiteral(resourceName: "Image6"), #imageLiteral(resourceName: "Image7"), #imageLiteral(resourceName: "Image8"), #imageLiteral(resourceName: "Image9"), #imageLiteral(resourceName: "Image10"), #imageLiteral(resourceName: "Image11"), #imageLiteral(resourceName: "Image12"), #imageLiteral(resourceName: "Image13"), #imageLiteral(resourceName: "Image14"), #imageLiteral(resourceName: "Image15"), #imageLiteral(resourceName: "Image16")]
+        
         for _ in 0..<4 {
             for _ in 0..<4 {
                 let blockFrame : CGRect = CGRect(x: 0, y: 0, width: blockWidth, height: blockWidth)
@@ -42,6 +46,8 @@ class ViewController: UIViewController {
                 let thisCenter : CGPoint = CGPoint(x: xCenter, y: yCenter)
                 
                 block.isUserInteractionEnabled = true
+                block.image = image[picNum]
+                picNum += 1
                 
                 block.center = thisCenter
                 block.originalCenter = thisCenter
