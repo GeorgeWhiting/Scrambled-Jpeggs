226c893596930e4403a535adb1b973b4d818b431
Calum-W <cal7907@hotmail.co.uk>
2018-02-09 10:18:17 +0000

    Add photo upload/take function (not yet functional)

--- Scrambled Jpeggs/ViewController.swift
+++ Scrambled Jpeggs/ViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var gameView: UIView!
     
@@ -52,6 +53,68 @@ class ViewController: UIViewController {
             }
             xCenter = blockWidth / 2
             yCenter = yCenter + blockWidth
+        }
+    }
+    
+    var newPic: Bool?
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func uploadImageTapped(_ sender: Any) {
+        let myAlert = UIAlertController(title: "Select image from", message: "", preferredStyle: .actionSheet)
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { (action) in
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+                let imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+                imagePicker.mediaTypes = [kUTTypeImage as String]
+                imagePicker.allowsEditing = false
+                self.present(imagePicker, animated: true, completion: nil)
+                self.newPic = true
+            }
+        }
+        
+        let cameraRollAction = UIAlertAction(title: "Camera Roll", style: .default) { (action) in
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+                let imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+                imagePicker.mediaTypes = [kUTTypeImage as String]
+                imagePicker.allowsEditing = false
+                self.present(imagePicker, animated: true, completion: nil)
+                self.newPic = false
+            }
+        }
+        
+        myAlert.addAction(cameraAction)
+        myAlert.addAction(cameraRollAction)
+        self.present(myAlert, animated: true)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let mediaType = info[UIImagePickerControllerMediaType] as! NSString
+        if mediaType.isEqual(to: kUTTypeImage as String) {
+            let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+            imageView.image = image
+            
+            if newPic == true {
+                UIImageWriteToSavedPhotosAlbum(image, self, #selector(imageError), nil)
+            }
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func imageError (image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo: UnsafeRawPointer) {
+        if error != nil {
+            let alert = UIAlertController(title: "Save failed", message: "Failed to save image", preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(cancelAction)
+            self.present(alert, animated: true, completion: nil)
         }
     }
 
