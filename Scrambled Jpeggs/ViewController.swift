f6afb4cd16293b24052388fbff74955e7b5a4061
Calum-W <cal7907@hotmail.co.uk>
2018-02-09 11:57:59 +0000

    Split uploaded images into an array of images and put them onto the puzzle blocks

--- Scrambled Jpeggs/ViewController.swift
+++ Scrambled Jpeggs/ViewController.swift
@@ -7,7 +7,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBOutlet weak var timerLabel: UILabel!
     
-    @IBOutlet weak var scrambleJpeggButton: UIButton!
     @IBAction func ResetButton(_ sender: Any) {
     }
     
@@ -19,13 +18,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     var blockArray: NSMutableArray = []
     var centersArray: NSMutableArray = []
-    var image: NSMutableArray = []
+    var images: [UIImage] = []
     var picNum : Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.scrambleJpeggButton.isHidden = true
-        image = [#imageLiteral(resourceName: "Image1"), #imageLiteral(resourceName: "Image2"), #imageLiteral(resourceName: "Image3"), #imageLiteral(resourceName: "Image4"), #imageLiteral(resourceName: "Image5"), #imageLiteral(resourceName: "Image6"), #imageLiteral(resourceName: "Image7"), #imageLiteral(resourceName: "Image8"), #imageLiteral(resourceName: "Image9"), #imageLiteral(resourceName: "Image10"), #imageLiteral(resourceName: "Image11"), #imageLiteral(resourceName: "Image12"), #imageLiteral(resourceName: "Image13"), #imageLiteral(resourceName: "Image14"), #imageLiteral(resourceName: "Image15"), #imageLiteral(resourceName: "Image16")]
+        images = [#imageLiteral(resourceName: "Image1"), #imageLiteral(resourceName: "Image2"), #imageLiteral(resourceName: "Image3"), #imageLiteral(resourceName: "Image4"), #imageLiteral(resourceName: "Image5"), #imageLiteral(resourceName: "Image6"), #imageLiteral(resourceName: "Image7"), #imageLiteral(resourceName: "Image8"), #imageLiteral(resourceName: "Image9"), #imageLiteral(resourceName: "Image10"), #imageLiteral(resourceName: "Image11"), #imageLiteral(resourceName: "Image12"), #imageLiteral(resourceName: "Image13"), #imageLiteral(resourceName: "Image14"), #imageLiteral(resourceName: "Image15"), #imageLiteral(resourceName: "Image16")]
         makeBlocks()
     }
     
@@ -49,7 +47,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 let thisCenter : CGPoint = CGPoint(x: xCenter, y: yCenter)
                 
                 block.isUserInteractionEnabled = true
-                block.image = (image[picNum] as! UIImage)
+                block.image = images[picNum]
                 picNum += 1
                 
                 block.center = thisCenter
@@ -71,8 +69,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     var newPic: Bool?
-    
-    @IBOutlet weak var imageView: UIImageView!
     
     @IBAction func uploadImageTaped(_ sender: Any) {
 
@@ -110,15 +106,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let mediaType = info[UIImagePickerControllerMediaType] as! NSString
         if mediaType.isEqual(to: kUTTypeImage as String) {
             let image = info[UIImagePickerControllerOriginalImage] as! UIImage
-            imageView.image = image
-            
+            images = slice(image: image, into: 4)
+            makeBlocks()
             
             if newPic == true {
                 UIImageWriteToSavedPhotosAlbum(image, self, #selector(imageError), nil)
-                self.scrambleJpeggButton.isHidden = false
             }
-            
-            self.scrambleJpeggButton.isHidden = false
         }
         self.dismiss(animated: true, completion: nil)
     }
@@ -135,9 +128,50 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             self.present(alert, animated: true, completion: nil)
         }
     }
-
-    @IBAction func scrambleJpeggTapped(_ sender: Any) {
+    
+    func slice(image: UIImage, into howMany: Int) -> [UIImage] {
+        let width: CGFloat
+        let height: CGFloat
         
+        switch image.imageOrientation {
+        case .left, .leftMirrored, .right, .rightMirrored:
+            width = image.size.height
+            height = image.size.width
+        default:
+            width = image.size.width
+            height = image.size.height
+        }
+        
+        let tileWidth = Int(width / CGFloat(howMany))
+        let tileHeight = Int(height / CGFloat(howMany))
+        
+        let scale = Int(image.scale)
+        var images = [UIImage]()
+        
+        let cgImage = image.cgImage!
+        
+        var adjustedHeight = tileHeight
+        
+        var y = 0
+        for row in 0 ..< howMany {
+            if row == (howMany - 1) {
+                adjustedHeight = Int(height) - y
+            }
+            var adjustedWidth = tileWidth
+            var x = 0
+            for column in 0 ..< howMany {
+                if column == (howMany - 1) {
+                    adjustedWidth = Int(width) - x
+                }
+                let origin = CGPoint(x: x * scale, y: y * scale)
+                let size = CGSize(width: adjustedWidth * scale, height: adjustedHeight * scale)
+                let tileCgImage = cgImage.cropping(to: CGRect(origin: origin, size: size))!
+                images.append(UIImage(cgImage: tileCgImage, scale: image.scale, orientation: image.imageOrientation))
+                x += tileWidth
+            }
+            y += tileHeight
+        }
+        return images
     }
     
 }
