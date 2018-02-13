7f6e7cbfc951bf8a0f20fb106eb40ddadc06190f
Xin Wang <wangxin00163@yahoo.co.uk>
2018-02-13 14:25:00 +0000

    Add UItests for musicToggole button

--- Scrambled JpeggsUITests/Scrambled_JpeggsUITests.swift
+++ Scrambled JpeggsUITests/Scrambled_JpeggsUITests.swift
@@ -1,11 +1,3 @@
-//
-//  Scrambled_JpeggsUITests.swift
-//  Scrambled JpeggsUITests
-//
-//  Created by george whiting on 08/02/2018.
-//  Copyright © 2018 george whiting. All rights reserved.
-//
-
 import XCTest
 
 class Scrambled_JpeggsUITests: XCTestCase {
@@ -13,24 +5,27 @@ class Scrambled_JpeggsUITests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+       
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSwipeButtonPlay() {
+        let app = XCUIApplication()
+        let firstSwitch = app.switches.element(boundBy: 0)
+        XCTAssertEqual(firstSwitch.value as! String, "1")
+    }
+    
+    func testSwipeButtonMute() {
+        let app = XCUIApplication()
+        let secondSwitch = app.switches.element(boundBy: 0)
+        XCUIApplication().switches["1"].tap()
+        XCTAssertEqual(secondSwitch.value as! String, "0")
     }
     
 }
