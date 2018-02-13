f9e743aed20fac0df0b7baa7984a510b0909436d
Xin Wang <wangxin00163@yahoo.co.uk>
2018-02-13 15:19:36 +0000

    Add UItests for "Easy, Medium and Difficulty" buttons

--- Scrambled JpeggsUITests/Scrambled_JpeggsUITests.swift
+++ Scrambled JpeggsUITests/Scrambled_JpeggsUITests.swift
@@ -1,14 +1,12 @@
 import XCTest
 
 class Scrambled_JpeggsUITests: XCTestCase {
+    let app = XCUIApplication()
         
     override func setUp() {
         super.setUp()
-        
-    
         continueAfterFailure = false
-       
-        XCUIApplication().launch()
+        app.launch()
     }
     
     override func tearDown() {
@@ -16,16 +14,31 @@ class Scrambled_JpeggsUITests: XCTestCase {
     }
     
     func testSwipeButtonPlay() {
-        let app = XCUIApplication()
         let firstSwitch = app.switches.element(boundBy: 0)
         XCTAssertEqual(firstSwitch.value as! String, "1")
     }
     
     func testSwipeButtonMute() {
-        let app = XCUIApplication()
         let secondSwitch = app.switches.element(boundBy: 0)
         XCUIApplication().switches["1"].tap()
         XCTAssertEqual(secondSwitch.value as! String, "0")
+    }
+    
+    func testEasyButton() {
+        let environment = app.segmentedControls.element(boundBy: 0);
+        app/*@START_MENU_TOKEN@*/.buttons["Easy"]/*[[".segmentedControls.buttons[\"Easy\"]",".buttons[\"Easy\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssertTrue(environment.buttons.element(boundBy:0).isSelected, "Easy");
+    }
+    
+    func testMediumButton() {
+        let environment = app.segmentedControls.element(boundBy: 0);
+        XCTAssertTrue(environment.buttons.element(boundBy:1).isSelected, "Medium");
+    }
+    
+    func testHardButton() {
+        let environment = app.segmentedControls.element(boundBy: 0);
+        app.buttons["Hard"].tap()
+        XCTAssertTrue(environment.buttons.element(boundBy:2).isSelected, "Hard");
     }
     
 }
